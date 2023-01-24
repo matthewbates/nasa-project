98f8fb5bb604ed83163de4538f4a489b904d58ea
Matthew Bates <matthewhcbates@gmail.com>
2023-01-24 16:50:40 -0700

    CRUD working

---
+++ Nasa/server/src/routes/launches/launches.test.js
@@ -0,0 +1,12 @@
+describe("TEST GET /launches", () => {
+  test("It should respond with 200 success", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("TEST POST /launches", () => {
+  test("It should respond with 200 success", () => {});
+  test("It should catch missing required properties", () => {});
+  test("It should catch invalid dates", () => {});
+});
