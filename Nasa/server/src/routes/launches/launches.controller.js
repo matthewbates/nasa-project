17c5a577f829f76a04ef8720479100e8fa7eecbd
Matthew Bates <matthewhcbates@gmail.com>
2023-01-24 18:29:31 -0700

    added tests for GET/POST /launches

--- Nasa/server/src/routes/launches/launches.controller.js
+++ Nasa/server/src/routes/launches/launches.controller.js
@@ -11,16 +11,18 @@ function httpGetLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
-  //   if (
-  //     !launch.mission ||
-  //     !launch.rocket ||
-  //     !launch.launchDate ||
-  //     !launch.target
-  //   ) {
-  //     return res.status(400).json({
-  //       error: "Missing required launch property",
-  //     });
-  //   }
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.target
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
   if (isNaN(launch.launchDate)) {
     return res.status(400).json({
